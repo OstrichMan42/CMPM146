df20651edb8da67f19e294bfd00374f0f31b0bf8
Caetano Hyams <cjhyams@ucsc.edu>
2020-01-28 23:48:06 -0800

    Copied vanilla to modified

--- p3/mcts_modified.py
+++ p3/mcts_modified.py
@@ -2,7 +2,7 @@
 Structure from
 https://www.geeksforgeeks.org/ml-monte-carlo-tree-search-mcts/
 """
-
+import random
 from mcts_node import MCTSNode
 from random import choice
 from math import sqrt, log
@@ -10,18 +10,31 @@ from math import sqrt, log
 num_nodes = 1000
 explore_faction = 2.
 
+ROLLOUTS = 10
+MAX_DEPTH = 5
+
+
+# Define a helper function to calculate the difference between the bot's score and the opponent's.
+def outcome(owned_boxes, game_points, me):
+    if game_points is not None:
+        # Try to normalize it up?  Not so sure about this code anyhow.
+        red_score = game_points[1]*9
+        blue_score = game_points[2]*9
+    else:
+        red_score = len([v for v in owned_boxes.values() if v == 1])
+        blue_score = len([v for v in owned_boxes.values() if v == 2])
+    return red_score - blue_score if me == 1 else blue_score - red_score
+
+
 
 def traverse_nodes(node, board, state, identity):
     """ Traverses the tree until the end criterion are met.
-
     Args:
         node:       A tree node from which the search is traversing.
         board:      The game setup.
         state:      The state of the game.
         identity:   The bot's identity, either 'red' or 'blue'.
-
     Returns:        A node from which the next stage of the search can proceed.
-
     """
     if node.untried_actions:
         return node
@@ -44,14 +57,11 @@ def traverse_nodes(node, board, state, identity):
 
 def expand_leaf(node, board, state):
     """ Adds a new leaf to the tree by creating a new child node for the given node.
-
     Args:
         node:   The node for which a child will be added.
         board:  The game setup.
         state:  The state of the game.
-
     Returns:    The added child node.
-
     """
     #print("untried actions: ", node.untried_actions)
     action = choice(node.untried_actions)
@@ -64,26 +74,57 @@ def expand_leaf(node, board, state):
 
 def rollout(board, state):
     """ Given the state of the game, the rollout plays out the remainder randomly.
-
     Args:
         board:  The game setup.
         state:  The state of the game.
     """
+
+    
     simState = state
-    while not board.is_ended(simState):
-        move = choice(board.legal_actions(simState))  # random choice
-        simState = board.next_state(simState, move)
+
+    moves = board.legal_actions(simState)
+
+    best_move = moves[0]
+    best_expectation = float('-inf')
+
+    me = board.current_player(simState)
+
+
+    for move in moves:
+        total_score = 0.0
+
+        # Sample a set number of games where the target move is immediately applied.
+        for r in range(ROLLOUTS):
+            rollout_state = board.next_state(simState, move)
+
+            # Only play to the specified depth.
+            for i in range(MAX_DEPTH):
+                if board.is_ended(rollout_state):
+                    break
+                rollout_move = random.choice(board.legal_actions(rollout_state))
+                rollout_state = board.next_state(rollout_state, rollout_move)
+
+            total_score += outcome(board.owned_boxes(rollout_state),
+                                    board.points_values(rollout_state), me)
+
+        expectation = float(total_score) / ROLLOUTS
+
+        # If the current move has a better average score, replace best_move and best_expectation
+        if expectation > best_expectation:
+            best_expectation = expectation
+            best_move = move
+            
+        #move = choice(board.legal_actions(simState))  # random choice
+        simState = board.next_state(simState, best_move)
 
     return board.points_values(simState)
 
 
 def backpropagate(node, won):
     """ Navigates the tree from a leaf node to the root, updating the win and visit count of each node along the path.
-
     Args:
         node:   A leaf node.
         won:    An indicator of whether the bot won or lost the game.
-
     """
     if node.parent is None:
         return
@@ -93,13 +134,10 @@ def backpropagate(node, won):
 
 def think(board, state):
     """ Performs MCTS by sampling games and calling the appropriate functions to construct the game tree.
-
     Args:
         board:  The game setup.
         state:  The state of the game.
-
     Returns:    The action to be taken.
-
     """
     identity_of_bot = board.current_player(state)
     root_node = MCTSNode(parent=None, parent_action=None, action_list=board.legal_actions(state))
